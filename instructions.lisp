a31504a3e7e60e5151c517497a06605840667f39
Nicky Nickell <pnqcad@gmail.com>
2018-03-16 14:21:28 -0800

    instructions will need to know the mnemonic the ck assembler expects. refinements to the target expansion of def-instruction

--- instructions.lisp
+++ instructions.lisp
@@ -1,10 +1,10 @@
 ;;;; Define the instructions of the papyrus VM
 
-(defmacro def-instruction (name &rest arguments) )
+(defmacro def-instruction (name asm &rest arguments) )
 (defmacro def-instructions (names &rest arguments) )
 
 (defstruct instruction
-  op
+  op asm
   dest dest-type
   arg1 arg1-type
   arg2 arg2-type
@@ -15,38 +15,56 @@
 
 ;;; def-instruction should expand into something like this. Note the slots for
 ;;; argument type. The type checker code will need these to generate the
-;;; appropriate casting for us. I don't like using keywords for this, but meh,
-;;; rough draft.
+;;; appropriate casting for us.
 
 (defstruct (integer-add
   (:include instruction
             (op 'integer-add)
-            (dest-type '(:integer-variable))
-            (arg1-type '(:integer-variable :integer-constant))
-            (arg2-type '(:integer-variable :integer-constant)))
-  (:constructor integer-add2 (dest arg1 arg2))
+            (asm 'iadd)
+            (dest-type +integer-variable+)
+            (arg1-type +integer-any+)
+            (arg2-type +integer-any+))
+  (:constructor integer-add (dest arg1 arg2))
   (:print-function
     (lambda (p s k)
-      (format s "~A " (instruction-op p))
+      (format s "~A " (instruction-asm p))
       (format s "~A, ~A, ~A"
               (instruction-dest p)
               (instruction-arg1 p)
               (instruction-arg2 p))))))
 
-; (def-instructions (integer-add integr-sub integer-mul integer-div integer-mod)
-;   dest arg1 arg2)
-;
-; (def-instruction integer-neg
-;   dest arg1)
-;
-; (def-instructions (float-add float-sub float-mul float-div)
-;   dest arg1 arg2)
-;
-; (def-instruction float-neg
-;   dest arg1)
-;
-; (define-instruction not
-;   dest arg1)
+(def-instructions
+  ((integer-add iadd)
+   (integer-sub isub)
+   (integer-mul imul)
+   (integer-div idiv)
+   (integer-mod imod))
+  (dest +integer-variable+)
+  (arg1 +integer-any+)
+  (arg2 +integer-any+))
+
+(def-instruction integer-neg ineg
+  (dest +integer-variable+)
+  (arg1 +integer-any+))
+
+(def-instructions
+  ((float-add fadd)
+   (float-sub fsub)
+   (float-mul fmul)
+   (float-div fdiv))
+  (dest +float-variable+)
+  (arg1 +float-any+)
+  (arg2 +float-any+))
+
+(def-instruction float-neg fneg
+  (dest +float-variable+)
+  (arg1 +float-any+))
+
+(def-instruction not not
+  (dest +bool-variable+)
+  (arg1 +any-any+))
+
+
 ;
 ; (def-instructions (jump-t jump-f)
 ;   target condition)
